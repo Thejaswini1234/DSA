5f1e47774c2c1adc3b6ec03893597744b5d395f3
Thejaswini1234 <chandruchaithra0@gmail.com>
2026-03-08 17:42:00 +0530

    Added Day5 Buy and Sell Stock problem

---
+++ Day5_Stocks/power.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+double myPow(double x,int n){
+    long binForm=n;
+
+    if(n<0){
+        x=1/x;
+        binForm=-binForm;
+    }
+
+    double ans=1;
+
+    while(binForm>0){
+        if(binForm%2==1){
+            ans*=x;
+        }
+        x*=x;
+        binForm/=2;
+    }
+    return ans;
+}
+int main(){
+    double x=2;
+    int n=5;
+
+    cout<<"Power = "<<myPow(x,n);
+    return 0;
+}
