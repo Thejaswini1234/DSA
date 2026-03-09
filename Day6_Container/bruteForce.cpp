d457753153b60e693fc2fe2fbd8a1dca26a9b440
Thejaswini1234 <chandruchaithra0@gmail.com>
2026-03-09 22:17:47 +0530

    Day6: Container With Most Water - Brute Force and Two Pointer Approach

---
+++ Day6_Container/bruteForce.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int maxArea(vector<int>heigth){
+    int maxWater=0;
+
+    for(int i=0;i<heigth.size();i++){
+        for(int j=i+1;j<heigth.size();j++){
+            int wd=j-i;
+            int ht=min(heigth[i],heigth[j]);
+            int currWater=wd*ht;
+
+            maxWater=max(maxWater,currWater);
+        }
+    }
+    return maxWater;
+};
+
+int main(){
+    vector<int>height={1,8,6,2,5,4,8,3,7};
+
+    cout<<"Maximum water:"<<maxArea(height)<<endl;
+    return 0;
+}
---
+++ Day6_Container/optimal.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int maxArea(vector<int>height){
+    int maxWater=0;
+    int lp=0,rp=height.size()-1;
+
+    while(lp<rp){
+        int wd=rp-lp;
+        int ht=min(height[lp],height[rp]);
+        int currWater=wd*ht;
+
+        maxWater=max(maxWater,currWater);
+
+        height[lp] < height[rp] ? lp++ :rp--;
+    }
+    return maxWater;
+};
+
+int main(){
+    vector<int>height={1,8,6,2,5,4,8,3,7};
+
+    cout<<"Maximum height is: "<<maxArea(height)<<endl;
+    return 0;
+}
