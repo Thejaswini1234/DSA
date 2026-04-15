0b94be90173158bd63cff416296949eefa4d3092
Thejaswini1234 <chandruchaithra0@gmail.com>
2026-04-15 15:34:40 +0530

    Added Single Element in Sorted Array solution (Binary Search)

---
+++ Day9_BinarySearch/5_single_element_sorted_array.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int singleNonDuplicate(vector<int>& A){
+    int n=A.size();
+
+    if(n==1) return A[0];
+
+    int st=0,end=n-1;
+
+    while(st<=end){
+        int mid=st+(end-st)/2;
+
+        //Edge cases
+        if(mid==0 && A[0]!=A[1]) return A[mid];
+        if(mid==n-1 && A[n-1]!=A[n-2]) return A[mid];
+
+        if(A[mid-1]!=A[mid] && A[mid]!=A[mid+1]) return A[mid];
+
+        if(mid%2==0){//even case
+            if(A[mid-1]==A[mid]){//left
+                end=mid-1;
+            }else{//right
+                st=mid+1;
+            }
+        }
+        else{//odd case
+            if(A[mid-1]==A[mid]){//right
+                st=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+    }
+    return -1;
+}
+int main(){
+    vector<int>A={1,1,2,2,3,4,4,5,5};
+
+    cout<<"Single Element is: "<<singleNonDuplicate(A);
+
+    return 0;
+}
